0df1eea895aa5d0deb127fc89616c773ce9f5479
Matheus Alves <mathalves@proton.me>
2022-09-07 22:58:33 -0300

    added websocket and message broadcasting

--- app.go
+++ app.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/go-redis/redis"
+	_ "github.com/go-redis/redis"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
 
-var (
-	db *redis.Client
-)
+type Message struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
+}
+
+// var (
+// 	db *redis.Client
+// )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+var clients = make(map[*websocket.Conn]bool)
+var broadcaster = make(chan Message)
 
 func main() {
 	err := godotenv.Load(".env")
@@ -20,14 +35,62 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	db = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0,
-	})
+	// db = redis.NewClient(&redis.Options{
+	// 	Addr:     os.Getenv("REDIS_ADDR"),
+	// 	Password: os.Getenv("REDIS_PASSWORD"),
+	// 	DB:       0,
+	// })
 
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/connect", handleConnection)
+
+	go handleMessages()
 
 	fmt.Println("Server started.")
 	log.Fatal(http.ListenAndServe(os.Getenv("SERVER_HOST")+":"+os.Getenv("SERVER_PORT"), nil))
+}
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Upgrade error: %v", err)
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg Message
+
+		err = ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("Reading error: %#v\n", err)
+			delete(clients, ws)
+			break
+		}
+
+		log.Printf("recv: message %q", msg)
+
+		broadcaster <- msg
+	}
+}
+
+// If a message is sent while a client is closing, ignore the error
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcaster
+
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil && unsafeError(err) {
+				log.Printf("Write error: %v", err)
+				delete(clients, client)
+				client.Close()
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/gorilla/websocket v1.5.0
 	github.com/joho/godotenv v1.4.0
 )
 
